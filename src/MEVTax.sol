1bddcb60dfc85fe7cfa0d5921d29f01ac7591a0a
Diego <105765223+0xfuturistic@users.noreply.github.com>
2024-06-11 15:40:20 -0700

    improve documentation, rename tax function to tax()

--- src/MEVTax.sol
+++ src/MEVTax.sol
@@ -9,51 +9,53 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 ///         The tax amount is calculated as a function of the priority fee per
 ///         gas of the transaction.
 contract MEVTax is Ownable {
-    /// @notice The ERC20 token used to pay the tax.
+    /// @notice ERC20 token for paying the tax.
     IERC20 public currency;
 
-    /// @notice The recipient of the tax payments.
+    /// @notice Recipient of the tax transfers.
     address public recipient = address(this);
 
-    /// @notice Modifier to apply tax on a function. If the paid amount (msg.value)
-    ///         is not enough to cover the tax, the modifier reverts.
+    /// @notice Modifier to apply tax on a function.
+    ///         If applying the tax fails, the modifier reverts.
     modifier applyTax() {
         _;
         _payTax();
     }
 
-    /// @notice Sets the deployer as the initial owner.
+    /// @dev Sets the deployer as the initial owner.
     constructor() Ownable(msg.sender) {}
 
-    /// @notice Sets the currency used to pay the tax.
-    /// @param _currency The new ERC20 token used to pay the tax.
+    /// @notice Returns the tax amount for _priorityFeePerGas according to
+    ///         tax function. Unless overridden, the tax function is 99 times
+    ///         the priority fee per gas.
+    /// @dev    Override this function to implement an arbitrary tax function.
+    /// @param  _priorityFeePerGas Priority fee per gas to input to the tax function.
+    /// @return Output of the tax function (tax amount for _priorityFeePerGas).
+    function tax(uint256 _priorityFeePerGas) internal view virtual returns (uint256) {
+        return 99 * _priorityFeePerGas;
+    }
+
+    /// @notice Updates currency to _currency.
+    /// @param _currency ERC20 token setting _currency to.
     function setCurrency(IERC20 _currency) external onlyOwner {
         // TODO: could enforce that the address is a valid ERC20 receiver
         currency = _currency;
     }
 
-    /// @notice Sets the recipient of the tax payments.
-    /// @param _recipient The new recipient of the tax payments.
+    /// @notice Updates recipient to _recipient.
+    /// @param _recipient Address setting recipient to.
     function setRecipient(address _recipient) external onlyOwner {
         recipient = _recipient;
     }
 
-    /// @notice Applies tax if the paid amount is sufficient to cover the tax.
-    ///         Otherwise, the function reverts.
+    /// @notice Applies tax by transferring the tax amount at the priority fee per gas
+    ///         from msg.sender to recipient. If the transfer fails, _payTax reverts.
     function _payTax() internal {
-        require(currency.transferFrom(msg.sender, recipient, _getTaxAmount()));
+        require(currency.transferFrom(msg.sender, recipient, tax(_getPriorityFeePerGas())));
     }
 
-    /// @notice Returns the tax amount, which is defined as a function of the
-    ///         priority fee per gas of the transaction.
-    /// @dev    This function should be overriden to implement custom tax function.
-    /// @return The tax amount as a function of the priority fee per gas.
-    function _getTaxAmount() internal view virtual returns (uint256) {
-        return _getPriorityFeePerGas() * 99;
-    }
-
-    /// @notice Returns the priority fee per gas of the transaction.
-    /// @return The priority fee per gas of the transaction.
+    /// @notice Returns the priority fee per gas.
+    /// @return Priority fee per gas.
     function _getPriorityFeePerGas() internal view returns (uint256) {
         return tx.gasprice - block.basefee;
     }
